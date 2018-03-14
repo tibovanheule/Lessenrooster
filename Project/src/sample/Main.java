ded4f65d8b8c10538c62438d93240a4ae26bff8b
Unknown <seventie@live.be>
2018-03-14 17:33:37 +0100

    arguments en error codes in readme

--- Project/src/sample/Main.java
+++ Project/src/sample/Main.java
@@ -1,13 +1,16 @@
+//Tibo Vanheule
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    //Exit code deze wordt doorgegeven aan System.exit() achter de Platform.exit de Javafx-processen heeft afgesloten
+    private static int exitCode = 0;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -18,6 +21,33 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        //Controle lengte argumenten
+        if (args.length == 0){
+            //Er zijn geen argumenten meegegeven, prog nrml opstarten
+            launch(args);
+        } else if (args.length == 1){
+            // TODO: 14/03/2018
+            // Moet geschreven worden naar std out
+            // dus vervangen door een writer
+            // system print is voor testing purposes
+            System.out.print("TEST");
+            //Programma afsluiten na het uitschrijven van de lijst.
+            Platform.exit();
+        } else if (args.length == 2){
+            // TODO: 14/03/2018
+            //openen van prog in met gewenst rooster
+            //werken met een default rooster en die dan overschrijven of (twee methoden)
+        }else if (args.length == 3 ){
+            // TODO: 14/03/2018
+            // nemen van een snapshot ???
+        }else if (args.length > 3 ){
+            // TODO: 14/03/2018
+            // foutboodschap
+            // is dat op stderr??? of met een venster
+            exitCode = 1;
+            Platform.exit();
+        }
+        System.exit(exitCode);
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Project lessenrooster
+#Project lessenrooster
+
+## By Tibo Vanheule
+
+## Error codes
+
+1. You have given more than 3 arguments! 
+...Notation: [student/location/teacher] [name teacher/student/location] [path]

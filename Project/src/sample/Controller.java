c2eb966800cfb9c1cb1076d9382297f1d80df225
Unknown <seventie@live.be>
2018-03-14 23:39:14 +0100

    Deel Design toegepast + eerste beginselen prog

--- Project/src/sample/Controller.java
+++ Project/src/sample/Controller.java
@@ -1,4 +1,36 @@
+//Tibo vanheule
 package sample;
 
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Controller {
+    public Label day;
+    public Label date;
+    public Label time;
+    public void initialize(){
+        // TODO: 14/03/2018  
+        //zet deze tijden  om in in oproepbare methode
+        //https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+        //de huidige datum
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("H:mm");
+        time.setText(now.format(formatTime));
+        //formatteer de datum (dag)
+        DateTimeFormatter formatDay = DateTimeFormatter.ofPattern("EEEE");
+        //geef nieuwe waarde aan de label met id:day
+        day.setText(now.format(formatDay));
+        //formateer datum
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("d LLLL");
+        //geef nieuwe waarde aan de label met id:date
+        date.setText(now.format(formatDate));
+    }
+    public void exit(){
+        Platform.exit();
+        System.exit(0);
+    }
+
 }
--- Project/src/sample/Main.java
+++ Project/src/sample/Main.java
@@ -7,15 +7,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
-    //Exit code deze wordt doorgegeven aan System.exit() achter de Platform.exit de Javafx-processen heeft afgesloten
-    private static int exitCode = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(root, 1000, 600);
+        scene.getStylesheets().add("Style.css");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -33,6 +36,7 @@ public class Main extends Application {
             System.out.print("TEST");
             //Programma afsluiten na het uitschrijven van de lijst.
             Platform.exit();
+            System.exit(0);
         } else if (args.length == 2){
             // TODO: 14/03/2018
             //openen van prog in met gewenst rooster
@@ -40,14 +44,17 @@ public class Main extends Application {
         }else if (args.length == 3 ){
             // TODO: 14/03/2018
             // nemen van een snapshot ???
-        }else if (args.length > 3 ){
+        }else if ( args.length > 3 ){
             // TODO: 14/03/2018
             // foutboodschap
-            // is dat op stderr??? of met een venster
-            exitCode = 1;
+            // is dat op stderr??? of met een gui
+            //Platform.exit om de Javafx-applicatie af te sluiten
             Platform.exit();
+            //stuur code naar methode exit in Exit klasse
+            //sluit Java Virtual Machine af met error code 1
+            System.exit(1);
         }
-        System.exit(exitCode);
+
 
     }
 }

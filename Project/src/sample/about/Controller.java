815d06a2963cee381219ffd28253e78ee6c8c476
Unknown <seventie@live.be>
2018-03-16 15:27:11 +0100

    algemene vooruitgang
    1. stages worden nu doorgegeven
    2. close aan about toegevoegt
    3. fout gefixt in aboutController (import vergeten)
    4. code gecleaned
    5. comments toegevoegd

--- Project/src/sample/about/Controller.java
+++
@@ -1,9 +0,0 @@
-package sample.about;
-
-import javafx.scene.Node;
-
-public class Controller {
-    public void close(){
-
-    }
-}
---
+++ Project/src/sample/about/aboutController.java
@@ -0,0 +1,21 @@
+package sample.about;
+
+import java.awt.*;
+import java.util.Properties;
+
+public class aboutController {
+    public Label version;
+    public Label mail;
+
+    /*
+    public void initialize() throws Exception{
+        Properties properties = new Properties();
+        properties.load(aboutController.class.getResourceAsStream("sample/myapp.properties"));
+        version.setText ("Version: " + properties.getProperty("version"));
+        mail.setText(properties.getProperty("email"));
+    }
+    */
+    public void close(){
+
+    }
+}

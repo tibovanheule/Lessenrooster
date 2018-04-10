587210e0dec5b46e5d75e37c780c130bfd921e9d
Tibo Vanheule <seventie@live.be>
2018-04-10 15:53:21 +0200

    edit lesen venster

--- Project/src/timetable/lecture/LectureController.java
+++ Project/src/timetable/lecture/LectureController.java
@@ -1,15 +1,23 @@
 package timetable.lecture;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import timetable.Main;
+import timetable.lecture.editLecture.EditLecture;
 import timetable.objects.Lecture;
-
 
 public class LectureController {
     public Label text, course;
     private Stage stage;
+    private Lecture lecture;
+    public Boolean canClose = true;
 
     public void setLecture(Lecture lecture) {
+        this.lecture = lecture;
         String[] days = {"monday", "tuesday", "wednesday", "thursday", "friday"};
         try {
 
@@ -19,8 +27,9 @@ public class LectureController {
                     + "Block: " + lecture.getBlock() + "\n"
                     + "Start hour" + lecture.getTime() + "\n"
                     + "For students: " + lecture.getStudent() + "\n"
-                    + "Teacher: " + lecture.getTeacher() + "\n");
-            //location.setText("test");
+                    + "Teacher: " + lecture.getTeacher() + "\n"
+                    + "Location: " + lecture.getLocation() + "\n"
+            );
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,7 +40,26 @@ public class LectureController {
         this.stage = stage;
     }
 
+    public void edit() {
+        try {
+            canClose = false;
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("lecture/editLecture/editLecture.fxml"));
+            Parent root = loader.load();
+            EditLecture controller = loader.getController();
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root, 450, 450));
+            controller.setStageAndSetupListeners(stage, lecture, this);
+            stage.show();
+        } catch (Exception e) {
+            canClose = true;
+            e.printStackTrace();
+        }
+    }
+
     public void close() {
-        stage.close();
+        if (canClose) {
+            stage.close();
+        }
     }
 }
--- Project/src/timetable/lecture/editLecture/EditLecture.java
+++ Project/src/timetable/lecture/editLecture/EditLecture.java
@@ -1,23 +1,41 @@
 package timetable.lecture.editLecture;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import timetable.lecture.LectureController;
 import timetable.objects.Lecture;
 
 public class EditLecture {
 
     private Stage stage;
     private Lecture lecture;
+    private LectureController controller;
+    public ComboBox<Integer> block,duration;
+    public ComboBox<String> teacher,students,day,loc;
+    public TextField name;
+    private static final String[] days = {"monday", "tuesday", "wednesday", "thursday", "friday"};
 
-    public void setStageAndSetupListeners(Stage stage, Lecture lecture) {
+    public void setStageAndSetupListeners(Stage stage, Lecture lecture, LectureController controller) {
         this.stage = stage;
         this.lecture = lecture;
+        this.controller = controller;
+        block.setValue(lecture.getBlock());
+        duration.setValue(lecture.getDuration());
+        teacher.setValue(lecture.getTeacher());
+        students.setValue(lecture.getStudent());
+        day.setValue(days[lecture.getDay()-1]);
+        loc.setValue(lecture.getLocation());
+
     }
 
     public void initialize(){
-
+        day.getItems().addAll(days);
     }
 
     public void close(){
+        // TODO: 10/04/2018 onderliggende stage realtime aanpassen
+        controller.canClose = true;
         stage.close();
     }
 }

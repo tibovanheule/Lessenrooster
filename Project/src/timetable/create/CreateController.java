490106bcfd65b628987b146874caba494db11203
Tibo Vanheule <seventie@live.be>
2018-04-22 12:28:52 +0200

    Edit lectures gefixt

---
+++ Project/src/timetable/create/CreateController.java
@@ -0,0 +1,45 @@
+//Tibo Vanheule
+package timetable.create;
+
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import timetable.config.Config;
+
+import java.util.Properties;
+
+public class AboutController {
+    public Label text;
+    private Stage stage;
+
+    public void setStageAndSetupListeners(Stage stage) {
+        //krijgen van de stage
+        this.stage = stage;
+    }
+
+    public void initialize() {
+
+        //inladen configuratie bestand
+        Config config = new Config();
+        Properties properties = config.getproperties();
+        //bewust alle tekst in 1 veld gestoken
+        //kleinere fxml en tekst staat dan ook altijd mooi onder elkaar, dankzij de new-line
+        text.setText("Version: " + properties.getProperty("program.version") +
+                "\nCopyright: " + properties.getProperty("programmer.name") +
+                "\nMade by: " + properties.getProperty("programmer.name") +
+                "\nEmail: " + properties.getProperty("programmer.email") +
+                "\nWebsite: " + properties.getProperty("programmer.site") +
+                "\nProjects github: " + properties.getProperty("program.github") +
+                "\nPrivate repo, send an email with github username for access!" +
+                "\nLayout (Collorpallet) based on: \n" + properties.getProperty("layout.basedOn") +
+                "\nWeather icons from:\n" + properties.getProperty("layout.weather.icons") +
+                "\nLatest compiled jar with all libraries available on " +
+                "\n" + properties.getProperty("programmer.site") + "/artifacts/"
+        );
+
+    }
+
+    public void close() {
+        //afsluiten stage
+        stage.close();
+    }
+}
